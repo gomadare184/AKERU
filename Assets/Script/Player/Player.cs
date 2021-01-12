ef9467696557100f06a7358fa1b175ce8674737d
gomadare <minecrafter1832810@gmail.com>
2021-01-13 00:25:29 +0900

    1

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class Player
+{
+    private ReactiveProperty<int> HP;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
