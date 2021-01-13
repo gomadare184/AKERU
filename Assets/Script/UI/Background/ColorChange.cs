b7725416caf2b0b6c3ba0d7e723f0ae6dcf800ac
gomadare <minecrafter1832810@gmail.com>
2021-01-14 03:14:01 +0900

    3

---
+++ Assets/Script/UI/Background/ColorChange.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ColorChange:MonoBehaviour
+{
+    [SerializeField]
+    private float daytime;
+    [SerializeField]
+    private float changetime;
+    [SerializeField]
+    private float DelayTime;
+
+    private Sequence sequence;
+    private Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sequence = DOTween.Sequence();
+        image = this.gameObject.GetComponent<Image>();
+
+//        sequence.Append(DOTween.To(
+//            () => image.color,                // 何を対象にするのか
+//            color => image.color = color,    // 値の更新
+//            Color.yellow,                    // 最終的な値
+//            daytime/6.0f  
+//            // アニメーション時間
+//        ).SetDelay(daytime/4.0f));
+
+//        sequence.Append(DOTween.To(
+//            () => image.color,                // 何を対象にするのか
+//            color => image.color = color,    // 値の更新
+//            Color.white,                    // 最終的な値
+//            daytime/6.0f                                // アニメーション時間
+//        ).SetDelay(daytime/4.0f));
+    }
+
+    public void CC_Black()
+    {
+        sequence.Append(DOTween.To(
+            () => image.color,                // 何を対象にするのか
+            color => image.color = color,    // 値の更新
+            Color.yellow,                    // 最終的な値
+            changetime                                // アニメーション時間
+        )); 
+            sequence.Append(DOTween.To(
+            () => image.color,                // 何を対象にするのか
+            color => image.color = color,    // 値の更新
+            Color.black,                    // 最終的な値
+            changetime                                // アニメーション時間
+        ));
+            
+                
+    }
+//    public void CC_yellow()
+//    {
+//        DOTween.To(
+//            () => image.color,                // 何を対象にするのか
+//            color => image.color = color,    // 値の更新
+//            Color.yellow,                    // 最終的な値
+//            changetime                                // アニメーション時間
+//        );
+//    }
+    public void CC_white()
+    {
+        DOTween.To(
+            () => image.color,                // 何を対象にするのか
+            color => image.color = color,    // 値の更新
+            Color.white,                    // 最終的な値
+            changetime                                // アニメーション時間
+        );
+    }
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/UI/Background/rotateSunMoon.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UniRx;
+using UniRx.Triggers;
+
+public class rotateSunMoon : MonoBehaviour
+{
+    [SerializeField]
+    private float speed=60;
+    private GameObject SkyColor;
+    private bool Blackchange;
+    private ColorChange CC;
+    private Sequence sequence;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sequence = DOTween.Sequence();
+        SkyColor=GameObject.Find("Sky");
+        CC = SkyColor.GetComponent<ColorChange>();
+        
+//        Observable.EveryUpdate()
+//            .Subscribe(
+//                _ => transform.Rotate(new Vector3(0,0,speed))
+//            );
+//        this.UpdateAsObservable()
+//            .Where(_ => gameObject.transform.rotation.z<=-80.0f )
+//            .Subscribe(_=> CC.CC_Black());
+//        this.UpdateAsObservable()
+//            .Where(_ => gameObject.transform.rotation.z==100.0f)
+//            .Subscribe(_=> CC.CC_white());
+        sequence.Append(gameObject.transform.DORotate(new Vector3(0f, 0, -360f), 3*speed/4, RotateMode.FastBeyond360)
+                .SetEase(Ease.Linear))
+
+            //.Append(gameObject.transform.DORotate(new Vector3(0f,0,-120f), speed/3, RotateMode.FastBeyond360).SetEase(Ease.Linear))
+            .InsertCallback(speed / 3, () => CC.CC_Black())
+            .InsertCallback(3*speed / 4, () => CC.CC_white())
+            .SetLoops(-1,LoopType.Restart);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
